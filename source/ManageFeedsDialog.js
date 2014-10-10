1f236e974b8d1491ec74f7de0707cb7afe3dfd47
Brent Hunter <aressel@gmail.com>
2014-10-10 14:34:13 -0700

    Initial rework of manage feeds dialog.

--- source/ManageFeedsDialog.js
+++ source/ManageFeedsDialog.js
@@ -6,7 +6,7 @@ enyo.kind({
     name: "ManageFeedsDialog",
     classes: "enyo-popup",
     //TODO: someone with more design skills than me should optimize that... :(
-    style: "padding: 30px; width: 90%; height: 90%;",
+    style: "padding: 15px; width: 90%; height: 90%;",
     kind: "onyx.Popup",
     //kind: "enyo.Control",
     centered: true,
@@ -17,11 +17,38 @@ enyo.kind({
     scrimWhenModal: false,
     components: [
         {kind: "Signals", onLoadFeedsFinished: "onFeeds" },
-        {
+        {name: "ManageFeedsScroller", touch: true, kind: "enyo.Scroller", style: "width: 100%;", components: [
+            {tag: "div", classes: "webosstyle-groupbox", components: [
+                {tag: "div", classes: "webosstyle-groupbox-header", content: $L("New Feed")},
+                {tag: "div", classes: "webosstyle-groupbox-body", style: "width: 100%", components:[
+					{kind: "enyo.FittableColumns", noStretch: true, classes: "settings-item", components: [
+						{kind: "onyx.InputDecorator", style: "width: 100%;", components: [
+							{kind: "onyx.Input", name: "newFeedName", style: "width: 100%; color: white", placeholder: $L("Name"), onfocus: "checkFocus", onblur: "checkBlur" }
+						]},
+					]},
+					{kind: "enyo.FittableColumns", noStretch: true, classes: "settings-item", components: [
+						{kind: "onyx.InputDecorator", style: "width: 100%;", components: [
+							{kind: "onyx.Input", name: "newFeedURL", style: "width: 100%;  color: white", placeholder: $L("URL"), value: "http://", onfocus: "checkFocus", onblur: "checkBlur" }
+						]},
+					]},
+					{kind: "enyo.FittableColumns", noStretch: true, classes: "settings-item", components: [
+						{tag: "span", classes: "settings-title-toggle", content: $L("Compressed"), fit: true},
+						{kind: "onyx.ToggleButton", name: "newFeedCompressedToggle"}
+					]},
+					{kind: "enyo.FittableColumns", noStretch: true, classes: "settings-item", style:"text-align: center", components: [
+						{name: "addNewFeedButton", kind: "onyx.Button", classes: "onyx-affirmative", content: $L("Add Feed"), ontap: "addNewFeed"}
+					]}
+                ]}
+            ]},
+        ]},
+        {tag: "div", style:"width: 100%; text-align: center", components: [
+        	{kind: "onyx.Button", classes: "onyx-affirmative", style: "margin:5px; width: 18%; min-width: 100px; font-size: 18px;", content: $L("Close"), ontap: "closePopup"}
+        ]}
+        /*{
             kind: "enyo.Scroller",
             horizontal: "hidden",
             classes: "enyo-fill",
-            style: "background-image:url('assets/bg.png')",
+            //style: "background-image:url('assets/bg.png')",
             touch: true,
             fit: true,
             components: [
@@ -44,11 +71,30 @@ enyo.kind({
                 ]}, //end of action group
                 {kind: "onyx.Button", style: "margin:5px;font-size:24px;float:center;", content: $L("Close"), ontap: "closePopup"}
             ]
-        }
+        }*/
     ],
     create: function (inSender, inEvent) {
         this.inherited(arguments);
     },
+    resizeHandler: function(){
+    	//Calculate scroller height - if we don't explicitly set the scroller height, it will overflow the dialog
+		var windowHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    	var dialogHeightPC = this.domStyles.height.replace('%', '') / 100;
+    	var dialogPadding = this.domStyles.padding.replace('px', '');
+
+		var scrollerHeight = Math.round(windowHeight * dialogHeightPC) - (dialogPadding * 2);
+
+    	this.$.ManageFeedsScroller.applyStyle("height", scrollerHeight + "px");
+
+		//Calculate Window Width
+		var windowWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    	var dialogWidthPC = this.domStyles.width.replace('%', '') / 100;
+    	
+    	var buttonWidth = Math.round(windowWidth * dialogWidthPC) - (dialogPadding * 2);
+		this.$.addNewFeedButton.applyStyle("width", buttonWidth + "px");
+		
+		this.inherited(arguments);
+	},
     //handlers
     onFeeds: function (inSender, inEvent) {
         var i;
@@ -69,5 +115,11 @@ enyo.kind({
             gzipped: this.$.newFeedCompressedToggle.getValue()
         };
         //TODO: what to do with new feed? urgs..
-    }
+    },
+    checkFocus: function(source, event) {
+		source.applyStyle("color", "black");
+	},
+	checkBlur: function(source, event) {
+		source.applyStyle("color", "white");
+	}
 });
